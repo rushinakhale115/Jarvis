62a45172658fd2d0487841db338d775f118c7486
rushinakhale115 <rushikesh.nakhale23@pccoepune.org>
2025-07-21 00:04:50 +0530

    startingsound

---
+++ engine/features.py
@@ -0,0 +1,8 @@
+from playsound import playsound
+
+
+#playing assistant sound
+def playAssistantSound():
+
+    music_dir="www\\assests\\audio\\www_assets_audio_start_sound.mp3"
+    playsound(music_dir)
--- www/main.js
+++ www/main.js
@@ -14,3 +14,28 @@ $(document).ready(function () {
 
 });
 
+//siri cinfiguration
+ var siriWave = new SiriWave({
+    container: document.getElementById("siri-container"),
+    width: 800,
+    height: 200,
+    style:"ios9",
+    amplitude:"1",
+    speed:"0.30",
+    autostart:true
+  });
+
+// Siri message animation
+    $('.siri-message').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "fadeInUp",
+            sync: true,
+        },
+        out: {
+            effect: "fadeOutUp",
+            sync: true,
+        },
+
+    });
